2c42300857fc00b9d1c5e474f4d89f65a3b17c43
sarassaura <sarahyukinonakada@gmail.com>
2024-01-13 19:21:40 -0300

    a

---
+++ useless/trimming.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+    int blank = 0;
+
+    c = getchar();
+
+    while (c != EOF) {
+        while (c == 32) {
+            c = getchar();
+            ++blank;
+        }
+        if (blank) {
+            putchar(32);
+            blank = 0;
+        }
+        putchar(c);
+        c = getchar();
+    }
+
+    return 0;
+}
