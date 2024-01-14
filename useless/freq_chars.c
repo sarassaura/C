41967677b7db858474da105e5f6643d6a9f455c9
sarassaura <sarahyukinonakada@gmail.com>
2024-01-14 09:17:39 -0300

    it oks

--- useless/freq_chars.c
+++ useless/freq_chars.c
@@ -5,16 +5,18 @@
 int main() {
     setlocale(LC_CTYPE, "en_US.UTF-8");
     int c;
-    int chars[128];
-    int freq[128];
+    int chars[129];
+    int freq[129];
     int columns;
     int lines;
 
     while (c != EOF) {
+        printf("\n");
+
         c = getchar();
         columns = lines = 0;
 
-        for (int i = 0; i < 128; i++) {
+        for (int i = 0; i < 129; i++) {
             chars[i] = 0;
             freq[i] = 0;
         }
@@ -24,7 +26,21 @@ int main() {
             c = getchar();
         }
 
-        for (int i = 0; i < 128; i++) {
+        printf("\n");
+
+        for (int i = 1; i < 129; i++) {
+            if (chars[i] != 0) {
+                printf("Char %c: ", i);
+                for (int j = 0; j < chars[i]; j++) {
+                    printf("%lc", (wint_t)9646);
+                }
+                printf(" %d charactere(s)\n", chars[i]);
+            }
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < 129; i++) {
             if (chars[i] != 0) {
                 freq[columns] = i;
                 if (chars[i] > lines) {
