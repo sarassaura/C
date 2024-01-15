6193df09b600edebd534d09ce0e81a5eb847b8e1
sarassaura <sarahyukinonakada@gmail.com>
2024-01-15 14:46:56 -0300

    dex

--- useless/lines_80_big.c
+++ useless/lines_80_big.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#define MAX 10
+
+void reset(char line[]);
 
 int main() {
-    printf("Hello Motto !");
+    char line[MAX];
+    int c = 0;
+    int length = 0;
+
+    while (c != EOF) {
+        c = getchar();
+        reset(line);
+        length = 0;
+
+        while (c != EOF && c != 10) {
+            line[length] = c;
+            ++length;
+            c = getchar();
+        }
+
+        if (length > 0 && length < MAX) {
+            printf("%s\n\n", line);
+        }
+    }
+
     return 0;
+}
+
+void reset(char line[]) {
+    for (int i = 0; i < MAX; i++) {
+        line[i] = 0;
+    }
 }
