b90c78a9741480c480a27b07db328fdf66687f12
sarassaura <sarahyukinonakada@gmail.com>
2024-01-25 20:44:43 -0300

    lbar

--- The C Programming Language/Chapter Two/var_ranges.c
+++ The C Programming Language/Chapter Two/var_ranges.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 
 unsigned long unsigned_convert(int size);
@@ -5,17 +6,29 @@ unsigned long unsigned_convert(int size);
 unsigned long half(int size);
 
 int main() {
-    printf("The range of signed char is:  -%lu to %lu\n", half(sizeof(char)), half(sizeof(char)) - 1);
-    printf("The range of signed short is: -%lu to %lu\n", half(sizeof(short)), half(sizeof(short)) - 1);
-    printf("The range of signed int is:   -%lu to %lu\n", half(sizeof(int)), half(sizeof(int)) - 1);
-    printf("The range of signed long is:  -%lu to %lu\n", half(sizeof(long)), half(sizeof(long)) - 1);
+    /*
+    printf("The range of signed char is:    -%lu to %lu\n", half(sizeof(char)), half(sizeof(char)) - 1);
+    printf("The range of signed short is:   -%lu to %lu\n", half(sizeof(short)), half(sizeof(short)) - 1);
+    printf("The range of signed int is:     -%lu to %lu\n", half(sizeof(int)), half(sizeof(int)) - 1);
+    printf("The range of signed long is:    -%lu to %lu\n", half(sizeof(long)), half(sizeof(long)) - 1);
+    */
+    printf("The range of signed char is:    %d to %d\n", CHAR_MIN, CHAR_MAX);
+    printf("The range of signed short is:   %d to %d\n", SHRT_MIN, SHRT_MAX);
+    printf("The range of signed int is:     %d to %d\n", INT_MIN, INT_MAX);
+    printf("The range of signed long is:    %ld to %ld\n", LONG_MIN, LONG_MAX);
 
     printf("\n");
 
+    /*
     printf("The range of unsigned char is:  0 to %lu\n", unsigned_convert(sizeof(char)));
     printf("The range of unsigned short is: 0 to %lu\n", unsigned_convert(sizeof(short)));
     printf("The range of unsigned int is:   0 to %lu\n", unsigned_convert(sizeof(int)));
     printf("The range of unsigned long is:  0 to %lu\n", unsigned_convert(sizeof(long)));
+    */
+    printf("The range of unsigned char is:  0 to %d\n", UCHAR_MAX);
+    printf("The range of unsigned short is: 0 to %d\n", USHRT_MAX);
+    printf("The range of unsigned int is:   0 to %ld\n", UINT_MAX);
+    printf("The range of unsigned long is:  0 to %lu\n", ULONG_MAX);
 
     return 0;
 }
