588d3195fb7421c18fe16da1b02ad867d2ea4460
sarassaura <sarahyukinonakada@gmail.com>
2024-01-25 21:17:24 -0300

    f

--- The C Programming Language/Chapter Two/var_ranges.c
+++ The C Programming Language/Chapter Two/var_ranges.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <limits.h>
 #include <stdio.h>
 
@@ -16,6 +17,8 @@ int main() {
     printf("The range of signed short is:   %d to %d\n", SHRT_MIN, SHRT_MAX);
     printf("The range of signed int is:     %d to %d\n", INT_MIN, INT_MAX);
     printf("The range of signed long is:    %ld to %ld\n", LONG_MIN, LONG_MAX);
+    printf("The range of signed float is:   %.1e to %.1e\n", -FLT_MAX, FLT_MAX);
+    printf("The range of signed double is:  %.1e to %.1e\n", -DBL_MAX, DBL_MAX);
 
     printf("\n");
 
