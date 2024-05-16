dcc5ad93ab70c99217f7ffb799421ca4552132d2
sarassaura <sarahyukinonakada@gmail.com>
2024-05-15 21:06:09 -0300

    mok

--- The C Programming Language/Chapter Two/var_ranges.c
+++ The C Programming Language/Chapter Two/var_ranges.c
@@ -6,54 +6,75 @@ unsigned long unsigned_convert(int size);
 
 unsigned long half(int size);
 
+long double pow2(int bits);
+
+long double fl();
+
+long double dfl();
+
 int main() {
-    /*
+
     printf("The range of signed char is:    -%lu to %lu\n", half(sizeof(char)), half(sizeof(char)) - 1);
     printf("The range of signed short is:   -%lu to %lu\n", half(sizeof(short)), half(sizeof(short)) - 1);
     printf("The range of signed int is:     -%lu to %lu\n", half(sizeof(int)), half(sizeof(int)) - 1);
     printf("The range of signed long is:    -%lu to %lu\n", half(sizeof(long)), half(sizeof(long)) - 1);
-    */
-    printf("The range of signed char is:    %d to %d\n", CHAR_MIN, CHAR_MAX);
-    printf("The range of signed short is:   %d to %d\n", SHRT_MIN, SHRT_MAX);
-    printf("The range of signed int is:     %d to %d\n", INT_MIN, INT_MAX);
-    printf("The range of signed long is:    %ld to %ld\n", LONG_MIN, LONG_MAX);
-    printf("The range of signed float is:   %.1e to %.1e with %d digits of precision\n", -FLT_MAX, FLT_MAX, FLT_DIG);
-    printf("The range of signed double is:  %.1e to %.1e with %d digits of precision\n", -DBL_MAX, DBL_MAX, DBL_DIG);
+    printf("The range of signed float is:   %.1Le to %.1Le\n", -fl(), fl());
+    printf("The range of signed double is:   %.3Le to %.3Le\n", -dfl(), dfl());
+    printf("The range of signed long double is:   %.2Le to %.2Le\n", -LDBL_MAX, LDBL_MAX);
+
+    // printf("The range of signed char is:    %d to %d\n", CHAR_MIN, CHAR_MAX);
+    // printf("The range of signed short is:   %d to %d\n", SHRT_MIN, SHRT_MAX);
+    // printf("The range of signed int is:     %d to %d\n", INT_MIN, INT_MAX);
+    // printf("The range of signed long is:    %ld to %ld\n", LONG_MIN, LONG_MAX);
+    // printf("The range of signed float is:   %.1e to %.1e with %d digits of precision\n", -FLT_MAX, FLT_MAX, FLT_DIG);
+    // printf("The range of signed double is:  %.1e to %.1e with %d digits of precision\n", -DBL_MAX, DBL_MAX, DBL_DIG);
+    // printf("The range of signed long double is:  %Le to %Le with %d digits of precision\n", -LDBL_MAX, LDBL_MAX,
+    // LDBL_DIG);
 
     printf("\n");
 
-    /*
     printf("The range of unsigned char is:  0 to %lu\n", unsigned_convert(sizeof(char)));
     printf("The range of unsigned short is: 0 to %lu\n", unsigned_convert(sizeof(short)));
     printf("The range of unsigned int is:   0 to %lu\n", unsigned_convert(sizeof(int)));
     printf("The range of unsigned long is:  0 to %lu\n", unsigned_convert(sizeof(long)));
-    */
-    printf("The range of unsigned char is:  0 to %d\n", UCHAR_MAX);
-    printf("The range of unsigned short is: 0 to %d\n", USHRT_MAX);
-    printf("The range of unsigned int is:   0 to %ld\n", UINT_MAX);
-    printf("The range of unsigned long is:  0 to %lu\n", ULONG_MAX);
+
+    // printf("The range of unsigned char is:  0 to %d\n", UCHAR_MAX);
+    // printf("The range of unsigned short is: 0 to %d\n", USHRT_MAX);
+    // printf("The range of unsigned int is:   0 to %ld\n", UINT_MAX);
+    // printf("The range of unsigned long is:  0 to %lu\n", ULONG_MAX);
 
     return 0;
 }
 
-unsigned long unsigned_convert(int size) {
-    int bits = size * 8;
-    unsigned long range = 1;
+long double pow2(int bits) {
 
-    for (int i = 0; i < bits; i++) {
-        range *= 2;
+    long double range = 1;
+    long double factor = 2;
+
+    if (bits < 0) {
+        factor = 1 / 2;
+        bits *= -1;
     }
 
-    return range - 1;
+    for (int i = 0; i < bits; i++) {
+        range *= factor;
+    }
+
+    return range;
+}
+
+unsigned long unsigned_convert(int size) {
+    int bits = size * 8;
+
+    return pow2(bits) - 1;
 }
 
 unsigned long half(int size) {
     int bits = size * 8;
-    unsigned long range = 1;
 
-    for (int i = 0; i < bits; i++) {
-        range *= 2;
-    }
-
-    return (((range - 2) / 2) + 1);
+    return pow2(bits - 1);
 }
+
+long double fl() { return pow2(127) * (2 - pow2(-23)); }
+
+long double dfl() { return pow2(1023) * (2 - pow2(-52)); }
